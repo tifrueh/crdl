3fe5ba6117428e395b38f19f0bbfd25382ad6bcf
Timo Früh <dev@tifrueh.ch>
2026-06-02 16:21:50 +0200

    Parse and embed metadata

--- crdl.sh
+++ crdl.sh
@@ -34,39 +34,46 @@ fi
 # Select correct URL.
 if [ "$1" = 'VM' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQz7D0l_eLJGAISVtcL7oRu_&si=_aR48fnGiVEruCYc'
-    prefix="C1 - Vox Machina E%(autonumber-1+${4})03d "
+    show='C1 - Vox Machina'
 elif [ "$1" = 'MN' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQxD0jjAE7PsWoaCrs0EkBH2&si=CxL0i_3ZTPQJ0YQ5'
-    prefix="C2 - Mighty Nein E%(autonumber-1+${4})03d "
+    show='C2 - Mighty Nein'
 elif [ "$1" = 'HB' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQydg3QOkBLG9OYqWJ0dwlxF&si=UiPOXygq34SIvulj'
-    prefix="C3 - Hells Bells - E%(autonumber-1+${4})03d "
+    show='C3 - Hells Bells'
 elif [ "$1" = 'EXU' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQzSnYHVT8X4pyMIbSX3i4gz&si=VAptRwwPPt2OwB7m'
-    prefix="Exandria Unlimited E%(autonumber-1+${4})03d "
+    show='Exandria Unlimited'
 elif [ "$1" = 'EXUC' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQwzhdskYekmjr0h2tsbKaZw&si=a8MxLSI_q7Sr_DAD'
-    prefix="Exandria Unlimited: Calamity - E%(autonumber-1+${4})03d "
+    show='Exandria Unlimited: Calamity'
 elif [ "$1" = 'EXUD' ]; then
     url='https://www.youtube.com/playlist?list=PL1tiwbzkOjQw_Q6CICX-9Rmoj2-OOvgPF'
-    prefix="Exandria Unlimited: Divergence - E%(autonumber-1+${4})03d "
+    show='Exandria Unlimited: Divergence'
 elif [ "$1" = 'AU' ]; then
     url='https://www.youtube.com/playlist?list=PL1tiwbzkOjQyLAwOfoBe6HjYZMnQbXNaZ'
-    prefix="Age of Umbra E%(autonumber-1+${4})03d "
+    show='Age of Umbra'
 else
     url="$1"
-    prefix=" - E%(autonumber-1+${4}03)d "
+    show="Unknown"
 fi
+
+prefix="${show} E%(autonumber-1+${4})03d "
 
 # Invoke yt-dlp.
 yt-dlp \
     --playlist-items "${2}:${3}" \
     --yes-playlist \
     --concurrent-fragments 6 \
-    --output "${prefix}%(title)s.%(ext)s" \
+    --output "${show} %(meta_title)s.%(ext)s" \
     --format "bv*+ba" \
     --write-subs \
     --sub-format 'srt' \
     --write-thumbnail \
+    --embed-metadata \
     --replace-in-metadata 'title' ' \| .* \| .* Episode [0-9]*' '' \
+    --parse-metadata "${show}:%(meta_show)s" \
+    --parse-metadata "%(autonumber+${4})03d:%(meta_episode_id)s" \
+    --parse-metadata "%(autonumber+${4})03d:%(meta_episode_sort)s" \
+    --parse-metadata "E%(meta_episode_id)03d %(title)s:%(meta_title)s" \
     $url
