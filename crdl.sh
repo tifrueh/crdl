8a2e8f5effd5d9b9b0838c637794f70c779b0179
Timo Früh <dev@tifrueh.ch>
2026-06-02 15:08:37 +0200

    Use new format
    * Don't force specific codec/container.
    * Store subtitles/thumbnail separately.
    * Improve title formatting.

--- crdl.sh
+++ crdl.sh
@@ -34,28 +34,28 @@ fi
 # Select correct URL.
 if [ "$1" = 'VM' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQz7D0l_eLJGAISVtcL7oRu_&si=_aR48fnGiVEruCYc'
-    prefix="Critical.Role.C1E%(autonumber-1+${4})03d."
+    prefix="C1 - Vox Machina E%(autonumber-1+${4})03d "
 elif [ "$1" = 'MN' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQxD0jjAE7PsWoaCrs0EkBH2&si=CxL0i_3ZTPQJ0YQ5'
-    prefix="Critical.Role.C2E%(autonumber-1+${4})03d."
+    prefix="C2 - Mighty Nein E%(autonumber-1+${4})03d "
 elif [ "$1" = 'HB' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQydg3QOkBLG9OYqWJ0dwlxF&si=UiPOXygq34SIvulj'
-    prefix="Critical.Role.C3E%(autonumber-1+${4})03d."
+    prefix="C3 - Hells Bells - E%(autonumber-1+${4})03d "
 elif [ "$1" = 'EXU' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQzSnYHVT8X4pyMIbSX3i4gz&si=VAptRwwPPt2OwB7m'
-    prefix="Critical.Role.EXU%(autonumber-1+${4})03d."
+    prefix="Exandria Unlimited E%(autonumber-1+${4})03d "
 elif [ "$1" = 'EXUC' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQwzhdskYekmjr0h2tsbKaZw&si=a8MxLSI_q7Sr_DAD'
-    prefix="Critical.Role.EXUC%(autonumber-1+${4})03d."
+    prefix="Exandria Unlimited: Calamity - E%(autonumber-1+${4})03d "
 elif [ "$1" = 'EXUD' ]; then
     url='https://www.youtube.com/playlist?list=PL1tiwbzkOjQw_Q6CICX-9Rmoj2-OOvgPF'
-    prefix="Critical.Role.EXUD%(autonumber-1+${4})03d."
+    prefix="Exandria Unlimited: Divergence - E%(autonumber-1+${4})03d "
 elif [ "$1" = 'AU' ]; then
     url='https://www.youtube.com/playlist?list=PL1tiwbzkOjQyLAwOfoBe6HjYZMnQbXNaZ'
-    prefix="Critical.Role.AU%(autonumber-1+${4})03d."
+    prefix="Age of Umbra E%(autonumber-1+${4})03d "
 else
     url="$1"
-    prefix="%(autonumber-1+${4}03)d."
+    prefix=" - E%(autonumber-1+${4}03)d "
 fi
 
 # Invoke yt-dlp.
@@ -64,15 +64,9 @@ yt-dlp \
     --yes-playlist \
     --concurrent-fragments 6 \
     --output "${prefix}%(title)s.%(ext)s" \
-    --format "bv*[vcodec~='^((he|a)vc|h26[45])']+ba[acodec~='^(aac|mp4a.*)']" \
-    --embed-subs \
+    --format "bv*+ba" \
+    --write-subs \
     --sub-format 'srt' \
-    --embed-thumbnail \
-    --convert-thumbnails 'png' \
-    --merge-output-format 'mp4' \
-    --remux-video 'mp4' \
-    --replace-in-metadata 'title' ' \| Critical Role(: .*)? \| (Campaign [0-9]*,? )?Episode [0-9]*' '' \
-    --exec "mv %(filepath)q '%(filepath)s.tmp'" \
-    --exec "ffmpeg -y -i '%(filepath)s.tmp' -map 0 -c:v copy -c:s copy ${volume_filter} -f mp4 %(filepath)q" \
-    --exec "rm '%(filepath)s.tmp'" \
+    --write-thumbnail \
+    --replace-in-metadata 'title' ' \| .* \| .* Episode [0-9]*' '' \
     $url
