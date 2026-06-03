957b67a2627fc18e8f87b4e6e0cd6d9fab2e7f83
Timo Früh <dev@tifrueh.ch>
2026-06-03 13:51:44 +0200

    Update format (again)
    * Conform with Jellyfin's "seasons".
    * Parse all title/episode information from the "title" field.

--- crdl.sh
+++ crdl.sh
@@ -2,13 +2,12 @@
 
 progname="$(basename $0)"
 
-help="usage: ${progname} <campaign> <from> <to> <autonumber>
+help="usage: ${progname} <campaign> <from> <to>
 
 options:
     campaign      The campaign to download from.
     from          The playlist item number of the first video.
     to            The playlist item number of the last video.
-    autonumber    The episode number of the first video.
 
 possible campaign values:
     VM            Campaign 1 – Vox Machina
@@ -21,7 +20,7 @@ possible campaign values:
     url           if none of the above, interpret as URL"
 
 # Check argument count and set volume filter if requested.
-if [ $# -lt 4 -o $# -gt 5 ]; then
+if [ $# -ne 3 ]; then
     printf '%s\n' "$help"
     exit 1
 fi
@@ -29,28 +28,28 @@ fi
 # Select correct URL.
 if [ "$1" = 'VM' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQz7D0l_eLJGAISVtcL7oRu_&si=_aR48fnGiVEruCYc'
-    show='Vox Machina'
+    season_nr='1'
 elif [ "$1" = 'MN' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQxD0jjAE7PsWoaCrs0EkBH2&si=CxL0i_3ZTPQJ0YQ5'
-    show='The Mighty Nein'
+    season_nr='2'
 elif [ "$1" = 'HB' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQydg3QOkBLG9OYqWJ0dwlxF&si=UiPOXygq34SIvulj'
-    show='Hells Bells'
+    season_nr='3'
 elif [ "$1" = 'EXU' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQzSnYHVT8X4pyMIbSX3i4gz&si=VAptRwwPPt2OwB7m'
-    show='Exandria Unlimited'
+    season_nr='51'
 elif [ "$1" = 'EXUC' ]; then
     url='https://youtube.com/playlist?list=PL1tiwbzkOjQwzhdskYekmjr0h2tsbKaZw&si=a8MxLSI_q7Sr_DAD'
-    show='Exandria Unlimited: Calamity'
+    season_nr='52'
 elif [ "$1" = 'EXUD' ]; then
     url='https://www.youtube.com/playlist?list=PL1tiwbzkOjQw_Q6CICX-9Rmoj2-OOvgPF'
-    show='Exandria Unlimited: Divergence'
+    season_nr='53'
 elif [ "$1" = 'AU' ]; then
     url='https://www.youtube.com/playlist?list=PL1tiwbzkOjQyLAwOfoBe6HjYZMnQbXNaZ'
-    show='Age of Umbra'
+    season_nr='54'
 else
     url="$1"
-    show="Unknown"
+    season_nr="NA"
 fi
 
 # Invoke yt-dlp.
@@ -58,15 +57,13 @@ yt-dlp \
     --playlist-items "${2}:${3}" \
     --yes-playlist \
     --concurrent-fragments 6 \
-    --output "%(meta_title)s.%(ext)s" \
-    --format "bv*+ba" \
+    --format 'bv*+ba' \
     --write-subs \
     --sub-format 'srt' \
     --write-thumbnail \
     --embed-metadata \
-    --replace-in-metadata 'title' ' \| .* \| .* Episode [0-9]*' '' \
-    --parse-metadata "${show}:%(meta_show)s" \
-    --parse-metadata "%(autonumber+${4})03d:%(meta_episode_id)s" \
-    --parse-metadata "%(autonumber+${4})03d:%(meta_episode_sort)s" \
-    --parse-metadata "E%(meta_episode_id)03d %(title)s:%(meta_title)s" \
+    --parse-metadata 'Critical Role:%(meta_show)s' \
+    --parse-metadata '%(title)s:(?P<title>.+) \|.*\|.*Episode (?P<episode_id>\d+).*' \
+    --parse-metadata "${season_nr}:%(season_number)s" \
+    --output 'Critical Role S%(season_number)02dE%(episode_id)03d %(title)s.%(ext)s' \
     $url
